8ad1e021d102632eb8f9fee345c99cbdb8e2eea1
trudova <liubovtrudova@gmail.com>
2021-10-11 14:57:00 -0400

    'get single job rout'

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,17 +1,35 @@
+const Job =require("../models/Job");
+const {StatusCodes} = require("http-status-codes");
+const {
+  UnauthenticatedError,
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+} = require("../errors");
+
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+const jobs = await Job.find({createdBy: req.user.userId}).sort("createdAt");
+res.status(StatusCodes.OK).json({jobs, count: jobs.length});
+
 };
 
 const getJob = async (req, res) => {
-  res.send("get all jobs");
+  const {user: {userId}, params:{id: jobId}} = req;
+  const job = await Job.findOne({_id: jobId, createdBy: userId});
+  if(!job){
+    throw new NotFoundError(`no job with id ${jobId} was found`)
+  }
+  res.status(StatusCodes.OK).json(job);
 };
 
 const createJob = async (req, res) => {
-  res.json(req.user)
+req.body.createdBy = req.user.userId;
+const job = await Job.create(req.body);
+res.status( StatusCodes.CREATED).json({job})
 };
 
 const updateJob = async (req, res) => {
-  res.send("get all jobs");
+  res.send("up all jobs");
 };
 
 const deleteJob = async (req, res) => {
